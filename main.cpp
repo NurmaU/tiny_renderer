4777fb393364731356d3dcc7de017801507ba2ad
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-28 17:54:37 +0600

    custom filling triangle

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image,
-          const TGAColor& color) {
+void line(Vec2i first, Vec2i second, TGAImage& image, const TGAColor& color) {
+    int x0 = first.x, y0 = first.y;
+    int x1 = second.x, y1 = second.y;
+
     bool steep = false;
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
@@ -40,7 +43,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image,
     }
 }
 
-int main(int argc, char** argv) {
+int draw_face() {
     int width = 1000;
     int height = 1000;
     TGAImage image(width, height, TGAImage::RGB);
@@ -63,7 +66,7 @@ int main(int argc, char** argv) {
             int x1 = (v1.x + 1.0) * width / 2.0;
             int y1 = (v1.y + 1.0) * height / 2.0;
 
-            line(x0, y0, x1, y1, image, white);
+            // line(x0, y0, x1, y1, image, white);
         }
     }
     image.flip_vertically();
@@ -77,4 +80,56 @@ int main(int argc, char** argv) {
         << " ms" << std::endl;
 
     return 0;
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+
+    int total_height = t2.y - t0.y;
+    int segment_height = t1.y - t0.y + 1;
+    for (int y = t0.y; y <= t1.y; y++) {
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t0.y) / segment_height;
+
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t0 + (t1 - t0) * beta;
+
+        if (A.x > B.x) std::swap(A, B);
+
+        for (int j = A.x; j <= B.x; j++) image.set(j, y, color);
+    }
+
+    segment_height = t2.y - t1.y + 1;
+    for (int y = t1.y; y <= t2.y; y++) {
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t1.y) / segment_height;
+
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t1 + (t2 - t1) * beta;
+
+        if (A.x > B.x) std::swap(A, B);
+
+        for (int j = A.x; j <= B.x; j++) image.set(j, y, color);
+    }
+}
+
+int main() {
+    Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    int width = 200;
+    int height = 200;
+    TGAImage image(width, height, TGAImage::RGB);
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+
+    image.flip_vertically();
+    image.write_tga_file("triangles.tga");
+
+    return 1;
 }
