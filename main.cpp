1a3eb02a553cf8aef161ae7ee946729145f7d389
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-28 15:25:55 +0600

    profiling

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
+#include <chrono>
 #include <iostream>
 #include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image,
-          const TGAColor& color) {
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
     bool steep = false;
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
@@ -30,10 +30,23 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image,
 
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white);
-    line(20, 13, 40, 80, image, red);
-    line(80, 40, 13, 20, image, red);
-    image.flip_vertically();
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 30000; i++) {
+        line(13, 20, 80, 40, image, white);
+        line(20, 13, 40, 80, image, red);
+        line(80, 40, 13, 20, image, red);
+        image.flip_vertically();
+    }
+
     image.write_tga_file("output.tga");
+    auto delta = std::chrono::high_resolution_clock::now() - start;
+
+    std::cout
+        << "Time spent: "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count()
+        << " ms" << std::endl;
+
     return 0;
 }
