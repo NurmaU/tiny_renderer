1942388a43559027bed5b79fbcf004ec94f1e621
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2022-12-29 15:33:12 +0600

    fix mouse

--- main.cpp
+++ main.cpp
@@ -43,35 +43,51 @@ void line(Vec2i first, Vec2i second, TGAImage& image, const TGAColor& color) {
     }
 }
 
-Vec3f baryCentric(Vec2i* pts, Vec2i P) {
-    Vec3f u = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x) ^
-              Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
-    if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+Vec3f baryCentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i = 2; i--;) {
+        s[i][0] = C[i] - A[i];
+        s[i][1] = B[i] - A[i];
+        s[i][2] = A[i] - P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2]) < 1e-2) return Vec3f(-1, 1, 1);
     return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
-void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
-    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
-    Vec2i bboxmax(0, 0);
-    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+void triangle(Vec3f* pts, float* zbuffer, TGAImage& image, TGAColor color) {
+    Vec2f bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2f bboxmax(0, 0);
+    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
 
     for (int i = 0; i < 3; i++) {
-        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+        bboxmin.x = std::max(0.f, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0.f, std::min(bboxmin.y, pts[i].y));
 
         bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
         bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
     }
 
-    Vec2i P;
+    Vec3f P;
 
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-            Vec3f bc_screen = baryCentric(pts, P);
+            Vec3f bc_screen = baryCentric(pts[0], pts[1], pts[2], P);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            image.set(P.x, P.y, color);
+
+            P.z = 0;
+            for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+            if (zbuffer[int(P.x + P.y * image.get_width())] < P.z) {
+                zbuffer[int(P.x + P.y * image.get_width())] = P.z;
+                image.set(P.x, P.y, color);
+            }
         }
     }
+}
+
+Vec3f world2screen(Vec3f v, int width, int height) {
+    return Vec3f(int((v.x + 1.) * width / 2. + .5),
+                 int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 int main() {
@@ -86,23 +102,26 @@ int main() {
     std::cout << "NUMBER OF VERTICES: " << model->nverts() << std::endl;
 
     Vec3f ligth_dir(0, 0, -1);
+    float* zbuffer = new float[width * height];
+    for (int i = width * height; i--;
+         zbuffer[i] = -std::numeric_limits<float>::max())
+        ;
 
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
+        Vec3f pts[3];
         Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
             Vec3f v = model->vert(face[j]);
-            screen_coords[j] =
-                Vec2i((v.x + 1) * width / 2.0, (v.y + 1) * height / 2.0);
             world_coords[j] = v;
+            pts[j] = world2screen(v, width, height);
         }
-        Vec3f n = (world_coords[2] - world_coords[0]) ^
-                  (world_coords[1] - world_coords[0]);
+        Vec3f n = cross((world_coords[2] - world_coords[0]),
+                        (world_coords[1] - world_coords[0]));
         n.normalize();
         float intensivity = n * ligth_dir;
         if (intensivity > 0)
-            triangle(screen_coords, image,
+            triangle(pts, zbuffer, image,
                      TGAColor(intensivity * 255, intensivity * 255,
                               intensivity * 255, 255));
     }
@@ -128,9 +147,9 @@ int demo() {
     int height = 200;
     TGAImage image(width, height, TGAImage::RGB);
 
-    triangle(t0, image, red);
-    triangle(t1, image, white);
-    triangle(t2, image, green);
+    // triangle(t0, image, red);
+    // triangle(t1, image, white);
+    // triangle(t2, image, green);
 
     image.flip_vertically();
     image.write_tga_file("triangles.tga");
