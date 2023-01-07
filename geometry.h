d86618b53e5a9a4013881829f9a077930e3720fa
nurmukhamed.ubaidullayev <nurmukhamed.ubaidullayev@giant.ai>
2023-01-07 14:39:16 +0600

    ref

--- geometry.h
+++ geometry.h
@@ -7,72 +7,90 @@
 
 class Matrix;
 
-template <class t>
-struct Vec2 {
-    t x, y;
-    Vec2<t>() : x(t()), y(t()) {}
-    Vec2<t>(t _x, t _y) : x(_x), y(_y) {}
-    Vec2<t> operator+(const Vec2<t>& V) const {
-        return Vec2<t>(x + V.x, y + V.y);
+template <class T>
+class Vec2 {
+   public:
+    T x, y;
+    Vec2<T>() : x(T()), y(T()) {}
+    Vec2<T>(T _x, T _y) : x(_x), y(_y) {}
+
+    Vec2<T> operator+(const Vec2<T>& V) const {
+        return Vec2<T>(x + V.x, y + V.y);
     }
-    Vec2<t> operator-(const Vec2<t>& V) const {
-        return Vec2<t>(x - V.x, y - V.y);
+
+    Vec2<T> operator-(const Vec2<T>& V) const {
+        return Vec2<T>(x - V.x, y - V.y);
     }
-    Vec2<t> operator*(float f) const { return Vec2<t>(x * f, y * f); }
-    t& operator[](const int i) { return i <= 0 ? x : y; }
+
+    Vec2<T> operator*(float f) const { return Vec2<T>(x * f, y * f); }
+
+    T& operator[](const int i) { return i <= 0 ? x : y; }
+
     template <class>
-    friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
+    friend std::ostream& operator<<(std::ostream& s, Vec2<T>& v);
 };
 
-template <class t>
-struct Vec3 {
-    t x, y, z;
-    Vec3<t>() : x(t()), y(t()), z(t()) {}
-    Vec3<t>(t _x, t _y, t _z) : x(_x), y(_y), z(_z) {}
-    Vec3<t>(Matrix m);
-    template <class u>
-    Vec3<t>(const Vec3<u>& v);
-    Vec3<t> operator^(const Vec3<t>& v) const {
-        return Vec3<t>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+template <class T>
+class Vec3 {
+   public:
+    T x, y, z;
+    Vec3<T>() : x(T()), y(T()), z(T()) {}
+    Vec3<T>(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+
+    Vec3<T>(Matrix m);
+
+    template <class U>
+    Vec3<T>(const Vec3<U>& v);
+
+    Vec3<T> operator^(const Vec3<T>& v) const {
+        return Vec3<T>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
     }
-    Vec3<t> operator+(const Vec3<t>& v) const {
-        return Vec3<t>(x + v.x, y + v.y, z + v.z);
+
+    Vec3<T> operator+(const Vec3<T>& v) const {
+        return Vec3<T>(x + v.x, y + v.y, z + v.z);
     }
-    Vec3<t> operator-(const Vec3<t>& v) const {
-        return Vec3<t>(x - v.x, y - v.y, z - v.z);
+
+    Vec3<T> operator-(const Vec3<T>& v) const {
+        return Vec3<T>(x - v.x, y - v.y, z - v.z);
     }
-    Vec3<t> operator*(float f) const { return Vec3<t>(x * f, y * f, z * f); }
-    t operator*(const Vec3<t>& v) const { return x * v.x + y * v.y + z * v.z; }
+
+    Vec3<T> operator*(float f) const { return Vec3<T>(x * f, y * f, z * f); }
+
+    T operator*(const Vec3<T>& v) const { return x * v.x + y * v.y + z * v.z; }
+
     float norm() const { return std::sqrt(x * x + y * y + z * z); }
-    Vec3<t>& normalize(t l = 1) {
+
+    Vec3<T>& normalize(T l = 1) {
         *this = (*this) * (l / norm());
         return *this;
     }
-    t& operator[](const int i) { return i <= 0 ? x : (1 == i ? y : z); }
+
+    T& operator[](const int i) { return i <= 0 ? x : (1 == i ? y : z); }
+
     template <class>
-    friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
+    friend std::ostream& operator<<(std::ostream& s, Vec3<T>& v);
 };
 
 typedef Vec2<float> Vec2f;
-typedef Vec2<int> Vec2i;
 typedef Vec3<float> Vec3f;
 typedef Vec3<int> Vec3i;
 
 template <>
 template <>
 Vec3<int>::Vec3(const Vec3<float>& v);
+
 template <>
 template <>
 Vec3<float>::Vec3(const Vec3<int>& v);
 
-template <class t>
-std::ostream& operator<<(std::ostream& s, Vec2<t>& v) {
+template <class T>
+std::ostream& operator<<(std::ostream& s, Vec2<T>& v) {
     s << "(" << v.x << ", " << v.y << ")\n";
     return s;
 }
 
-template <class t>
-std::ostream& operator<<(std::ostream& s, Vec3<t>& v) {
+template <class T>
+std::ostream& operator<<(std::ostream& s, Vec3<T>& v) {
     s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n";
     return s;
 }
